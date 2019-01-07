26affd7c0d22125d1ba318c015db32e14813ff19
cdzhengwujiang <cdzhengwujiang@jd.com>
2019-01-07 10:11:18 +0800

    style update

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function md2html(mdStr) {
             const res = codeHtml.value.replace(/^(\s+)/g, function ($, $1) {
                 return new Array($1.length + 1).join('&nbsp;');
             });
-            return `<p style="margin: 3px 0;padding: 0 10px;border-width: 0px;border-style: initial;border-color: initial;line-height:16px;font-size:14px;text-align: justify;word-break: break-all;overflow-wrap: break-word;hyphens: auto;white-space: nowrap !important;">${res}</p>`;
+            return `<p style="margin: 3px 0;border-width: 0px;border-style: initial;border-color: initial;line-height:16px;font-size:14px;text-align: justify;word-break: break-all;overflow-wrap: break-word;hyphens: auto;white-space: nowrap !important;"><span style="margin: 0 10px;white-space: nowrap !important;">${res}</span></p>`;
         });
         return `<blockquote style="padding: 10px 0;border-width: 0px;border-style: initial;border-color: initial;color: rgb(51, 51, 51);white-space: normal;overflow:auto;">
             ${rowsHtml.join('')}
